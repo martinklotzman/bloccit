1bd5ec84533a455bf64ddbaf65026f91f80c8377
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-11-12 17:29:29 -0800

    second commit

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,4 +6,42 @@ class TopicsController < ApplicationController
   def show
     @topic = Topic.find(params[:id])
   end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+    @topic.public = params[:topic][:public]
+
+    if @topic.save
+      redirect_to @topic, notice: "Topic was saved successfully."
+    else
+      flash.now[:alert] = "Error creating topic. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+    @topic.public = params[:topic][:public]
+
+    if @topic.save
+      flash[:notice] = "Topic was updated."
+      redirect_to @topic
+    else
+      flash.now[:alert] = "Error saving topic. Please try again."
+      render :edit
+    end
+  end
 end
