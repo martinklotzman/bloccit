86466ebbd23f2e87b5ce22ce2395ed2a300d5aec
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-11-28 20:33:09 -0800

    checkpoint 26

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,11 +12,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
     @topic = Topic.find(params[:topic_id])
-    @post.topic = @topic
+    @post = @topic.posts.build(post_params)
     @post.user = current_user
 
 
@@ -35,8 +32,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+    @post.assign_attributes(post_params)
 
     if @post.save
       flash[:notice] = "Post was updated."
@@ -57,5 +53,10 @@ class PostsController < ApplicationController
       flash.now[:alert] = "There was an error deleting the post."
       render :show
     end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
