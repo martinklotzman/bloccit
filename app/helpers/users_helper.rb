97b157eaca292caa134d177c289775efca5e59aa
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-12-15 15:26:21 -0800

    checkpoint 31 redo

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def posts_comments_view(association)
+    if @user.send(association).any?
+      render @user.send(association)
+    else
+       content_tag(:p, "#{@user.name} has not submitted any #{association.to_s}")
+     end
+  end
 end
